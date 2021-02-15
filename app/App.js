6f014cc24072634c338d26a3d83658d895b58b8d
Charles van Lennep <charles@vanlennep.co.uk>
2021-02-15 11:44:46 +0000

    create barebones app with dependencies

---
+++ app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView></SafeAreaView>
+    </React.Fragment>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
